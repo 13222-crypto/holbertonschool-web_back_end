9829a1510a050561481d32862d775774cc92f0f8
13222-crypto <13222@holbertonstudents.com>
2026-09-19 14:06:39 +0300

    Create 8-all.py

---
+++ NoSQL/8-all.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+Module 8-all
+Contains function to list all documents in a PyMongo collection.
+"""
+
+
+def list_all(mongo_collection):
+    """
+    Lists all documents in a collection.
+
+    :param mongo_collection: pymongo collection object
+    :return: list of documents, or empty list if collection is empty
+    """
+    if mongo_collection is None:
+        return []
+    return list(mongo_collection.find())
