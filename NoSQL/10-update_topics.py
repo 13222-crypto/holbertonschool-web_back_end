f9134e79113de77a91e429c11ced6be672d2d7c8
13222-crypto <13222@holbertonstudents.com>
2026-09-19 13:29:28 +0300

    Create 10-update_topics.py

---
+++ NoSQL/10-update_topics.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+Module 10-update_topics
+Contains function to update topics of a school document based on name.
+"""
+
+
+def update_topics(mongo_collection, name, topics):
+    """
+    Changes all topics of a school document based on the name.
+
+    :param mongo_collection: pymongo collection object
+    :param name: (string) school name to update
+    :param topics: (list of strings) list of topics approached in the school
+    :return: None
+    """
+    mongo_collection.update_many(
+        {"name": name},
+        {"$set": {"topics": topics}}
+    )
